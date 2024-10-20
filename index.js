07bd784092449398b423a35bfe9a792fa6a4380b
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-20 12:13:42 +0200

    goodByeFunction

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return 'S úctou ' + name;
+};
+
+const rudeGoodbye = (name) => {
+  return 'Zdar vole a měj se ' + name;
+};
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+fillSubject('Obchodní sdělení');
+fillBody(
+  'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+  'Jan Čistý',
+  formalGoodbye);
+
+/*fillSubject('Pozvánka na oslavu narozenin');
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye);*/
